ca0c89161b2878b0f8127a060b91f5afc0049829
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-11-03 02:29:04 +0900

    test(domain): ''로또 도메인 테스트 추가"

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import model.domain.Lotto;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+
+class LottoTest {
+    @Test
+    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호의_개수가_6개보다_적다면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호의_범위는_1부터_45_사이어야_한다(){
+        assertThatThrownBy(() -> new Lotto(List.of(0, 46, 1, 2, 3, 4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
