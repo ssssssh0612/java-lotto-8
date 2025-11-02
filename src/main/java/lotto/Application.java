e498b350b4f35dcda5232f639186be7a30e33f8a
sibjagun <sibjagun99@kyungmin.ac.kr>
2025-11-03 02:49:34 +0900

    feat: "로직 완성"

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import model.domain.BonusNumber;
+import model.domain.LottoRankResult;
+import model.domain.Lottos;
+import model.domain.PurchaseAmount;
+import model.domain.WinningLotto;
+import model.domain.WinningNumbers;
+import model.service.LottoMakerService;
+import model.service.LottoRankResultMakerService;
+import view.BonusNumberInput;
+import view.InputString;
+import view.OutputView;
+import view.PurchaseAmountInput;
+import view.WinningNumbersInput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputString<PurchaseAmount> purchaseAmountInput = new InputString<>(new PurchaseAmountInput());
+        PurchaseAmount purchaseAmount = purchaseAmountInput.getInputString(Console::readLine);
+        OutputView.printView(purchaseAmount);
+        LottoMakerService lottoMakerService = new LottoMakerService();
+        Lottos lottos = lottoMakerService.makeLottos(purchaseAmount.purchasableCount());
+        OutputView.printView(lottos);
+        InputString<WinningNumbers> winningNumbersInput = new InputString<>(new WinningNumbersInput());
+        WinningNumbers winningNumbers = winningNumbersInput.getInputString(Console::readLine);
+        InputString<BonusNumber> bonusNumberInput = new InputString<>(new BonusNumberInput(winningNumbers));
+        BonusNumber bonusNumber = bonusNumberInput.getInputString(Console::readLine);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+        LottoRankResultMakerService lottoRankResultMakerService = new LottoRankResultMakerService();
+        LottoRankResult lottoRankResult = lottoRankResultMakerService.makeLottoRankResult(winningLotto, lottos);
+        OutputView.printView(lottoRankResult);
     }
 }
